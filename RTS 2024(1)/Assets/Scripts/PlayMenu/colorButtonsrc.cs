89346f727b8458f38e01ada370d68909356eccfe
chuhandorik <armenbogomolov08@mail.ru>
2024-10-31 23:19:22 +0300

    Create Enemy Button
    Instan

--- RTS 2024(1)/Assets/Scripts/PlayMenu/colorButtonsrc.cs
+++ RTS 2024(1)/Assets/Scripts/PlayMenu/colorButtonsrc.cs
@@ -3,41 +3,63 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
+
+
 public class colorButtonsrc : MonoBehaviour
 {
     // Start is called before the first frame update
-    public int o;
-    [SerializeField] Button but;
+
+    public Button but;
+    public int index;
+    public int[,] list;
+   
+
     void Start()
     {
-        switch (o)
-        {
-            case 0:
-                but.image.color = Color.white; break;
-            case 1:
-                but.image.color = Color.black; break;
-            case 2:
-                but.image.color = Color.red; break;
-            case 3:
-                but.image.color = Color.green; break;
-            case 4:
-                but.image.color = Color.blue; break;
-            case 5:
-                but.image.color = Color.yellow; break;
-        }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     public void Swin()
     {
-        o += 1;
-        if (o == 6) o = 0;
-        Start();
-        
-    }
+        //bool i = false;
+        //while (true)
+        //{
+        //    for (int j = 0; j < 6; j++)
+        //    {
+        //        if (!list_col[j].active) { but.image.color = list_col[j].color; i = true; list_col[j].active = true; }
+                
+        //    }
+        //    if (i) { break; }
+
+        //}
+        }
+    //if (!listb[index])
+    //{
+
+    //    
+    //    //if (index != 5)
+    //    //{
+
+    //    //    but.image.color = new Color(list[index, 0], list[index, 1], list[index, 2]);
+    //    //    listb[index] = false;
+    //    //    index += 1;
+    //    //    listb[index] = true;
+
+    //    //}
+    //    //else { 
+    //    //    but.image.color = new Color(list[index, 0], list[index, 1], list[index, 2]);
+    //    //    index = 0;
+    //    //}
+    //}
+
+    //else  { index += 1; Swin(); }
+    //}
+    
 
 }
